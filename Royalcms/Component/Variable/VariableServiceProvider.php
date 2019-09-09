4e365f630b8983bb464e5e355be32480fcc5eb99
王正东 <wangzhengdong@ecmoban.com>
2019-09-09 14:55:40 +0800

    更新服务注册单例的方法

--- Royalcms/Component/Variable/VariableServiceProvider.php
+++ Royalcms/Component/Variable/VariableServiceProvider.php
@@ -18,7 +18,7 @@ class VariableServiceProvider extends ServiceProvider {
 	 */
 	public function register()
 	{
-		$this->royalcms->bindShared('variable', function($royalcms)
+		$this->royalcms->singleton('variable', function($royalcms)
 		{
 			return new Variable;
 		});
